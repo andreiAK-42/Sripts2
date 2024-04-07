5ad0267320d5cfbc05279d70cdf6545f0ba9d3e7
andreiAK-42 <145548192+andreiAK-42@users.noreply.github.com>
2024-04-07 22:40:43 +0700

    1

--- script1.sh
+++ script1.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-    echo "Использование: $0 <directory>"
-    exit 1
+# Проверяем наличие аргумента
+if [ -z "$1" ]; then
+  echo "Укажите путь к каталогу в качестве аргумента"
+  exit 1
 fi
 
+# Проверяем существование каталога
 if [ ! -d "$1" ]; then
-    echo "Ошибка: Директория $1 не найдена."
-    exit 1
+  echo "Указанный каталог не существует"
+  exit 1
 fi
 
-echo "Что вывести в список? (files/directories)"
-read choice
-
-if [ "$choice" == "files" ]; then
-    echo "Файлы в $1:"
-    ls -p "$1" | grep -v / 
-elif [ "$choice" == "directories" ]; then
-    echo "Директории в $1:"
-    ls -p "$1" | grep / 
+# Проверяем выбор пользователя
+if [ "$2" == "files" ]; then
+  # Выводим список файлов
+  ls -p "$1" | grep -v / 
+elif [ "$2" == "directories" ]; then
+  # Выводим список каталогов
+  ls -p "$1" | grep / | sed 's/\/$//'
 else
-    echo "Неверное значение. Используйте 'files' или 'directories'."
-    exit 1
+  echo "Выберите что вы хотите получить (files или directories)"
+  exit 1
 fi
